90bae1ff6a0518f431d0548b03322d2a0b16252b
Piotr Sztajgla <sztajglaP@interia.eu>
2022-10-19 21:00:51 +0200

    created info section

--- dist/ts/script.js
+++ dist/ts/script.js
@@ -1,8 +1,10 @@
 var navButton = document.querySelector('.nav__burger-btn');
 var navMenu = document.querySelector('.nav__menu');
 var nav = document.querySelector('.nav');
+var headerArrowBtn = document.querySelector('.header__arrow-btn');
 var showNavMenu = function () {
     navMenu.classList.toggle('active');
+    headerArrowBtn.classList.toggle('active');
 };
 var addShadowOnNav = function () {
     if (scrollY > 0) {
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,9 +1,11 @@
 const navButton: HTMLButtonElement = document.querySelector('.nav__burger-btn');
 const navMenu: HTMLElement = document.querySelector('.nav__menu');
 const nav: HTMLElement = document.querySelector('.nav');
+const headerArrowBtn: HTMLButtonElement = document.querySelector('.header__arrow-btn');
 
 const showNavMenu = () => {
     navMenu.classList.toggle('active');
+    headerArrowBtn.classList.toggle('active');
 }
 
 const addShadowOnNav = () => {
